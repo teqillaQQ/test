36eaff2c46ffb69cf11b51afea2558c134583a0b
Alexander <polskibandit@gmail.com>
2021-11-12 15:24:50 +0300

    Сделал подсветку кнопок

---
+++ cakculator3/cakculator3/BinaryOperatorButton.swift
@@ -0,0 +1,12 @@
+//
+//  BinaryOperatorButton.swift
+//  cakculator3
+//
+//  Created by Александр Савков on 6.11.21.
+//
+
+import UIKit
+
+class BinaryOperatorButton: RoundedButton {
+    var operatorType: BinaryOperand?
+}
